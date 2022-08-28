49a901c53765a23253ba3d3978cbcb49ebad3f8a
André Luiz Melicio <71500309+Coffee4-4@users.noreply.github.com>
2022-08-28 14:03:00 -0300

    Desafio finalizado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,27 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    let id = new Date().getTime()
+    setTasks([...tasks, { id: id ,title: newTaskTitle, done: false }]);
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    setTasks(tasks.map(task => task.id === id ? { ...task, done: !task.done } : task));
+    
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task=> task.id !== id))
+
   }
 
   return (
